cc74ee685216f2a39ef6e6a9f657123b6bb0d658
ChaitraSatyanarayana <32605952+ChaitraSatyanarayana@users.noreply.github.com>
2018-05-03 15:54:08 -0700

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,19 +1,42 @@
 //This files creates the database and tables required
+// it creates public and private key for purchase center, supervisor and order department and stores it in database
+
 import MySQLdb
+import rsa
+from email_credentials import *
+import cPickle as pickle
+
+def create_keys(email,pw):
+     (public,private) = rsa.newkeys(2048)
+     public_str = pickle.dumps(public)
+     private_str = pickle.dumps(private)
+     db = MySQLdb.connect(host="localhost",user="root",passwd="Welcome1")
+     cursor = db.cursor() 
+     cursor.execute("""use spo""")
+     cursor.execute("""INSERT INTO Cred VALUES (%s,%s,%s)""",(email,pw,private_str))    #insert credentials
+     db.commit()
+     cursor.execute("""INSERT INTO PublicKeyDir VALUES (%s,%s)""",(email,public_str))         #insert public key in public key directory 
+     db.commit()
+     cursor.close()
+     db.close()
 
 db = MySQLdb.connect(host="localhost",user="root",passwd="Welcome1")
 cursor = db.cursor()
-cursor.execute("""Create database spo""")
+cursor.execute("""Create database spo""") #create student purchase order database 
 cursor.execute("""use spo""")
-cursor.execute("""CREATE TABLE Cred (
+cursor.execute("""CREATE TABLE Cred (      
     Email varchar(255),
     Password varchar(255),
     PrivateKey varchar(5000)
-    )""")
+    )""")        #create Cred Table
 
 cursor.execute("""CREATE TABLE PublicKeyDir (
     Email varchar(255),
     PublicKey varchar(5000)
-    )""")
+    )""")       #create Public Key directory table 
+
+create_keys(purchasecenter_mail,purchasecenter_password)  #create keys for purchase center and store it in database 
+create_keys(supervisor_mail,supervisor_password)   #create keys for supervisor and store it in database 
+create_keys(orderdepartment_mail,orderdepartment_password)                   #create keys for order department and store it in database 
 cursor.close()
 db.close()
